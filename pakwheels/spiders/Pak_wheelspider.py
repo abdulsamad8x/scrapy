665ba75959bba934345f4a23c6e4e8e443d8c346
abdulsamad8x <abdulsamad8x@gmail.com>
2024-09-30 22:36:05 +0500

    4th day

--- pakwheels/spiders/Pak_wheelspider.py
+++ pakwheels/spiders/Pak_wheelspider.py
@@ -7,8 +7,10 @@ class ExampleSpider(scrapy.Spider):
     def parse(self, response):
         title = response.css('div.col-md-6.line.or li::text').getall()
         title2 = response.css('div.col-md-6 li::text').getall()
+        title3 = response.css('div.col-md-6.mb20 a::attr(href)')[1].getall()
         
         yield {
             'title': title,
-            'title2' : title2
+            'title2': title2,
+            'title3': title3,
         }
